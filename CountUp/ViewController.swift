29f235dc6242a453019e2ac4976f5fdfb8987d51
Ryogo Maeda <ryogo.m1221@gmail.com>
2020-03-29 22:56:48 +0900

    finish

--- CountUp/ViewController.swift
+++ CountUp/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var count = 0
+    
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func plus(_ sender: Any) {
+        
+        count = count + 1
+        label.text = String(count)
+    }
+    
+    @IBAction func minus(_ sender: Any) {
+        count = count - 1
+        if count < 0 {
+            count = 0
+        }
+        label.text = String(count)
+    }
 }
 
